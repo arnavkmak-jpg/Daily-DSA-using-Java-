f11e5a685b0263b60c6abdff916845e870328688
arnavkmak-jpg <arnavkmak@gmail.com>
2026-08-11 12:28:26 +0530

    11. Container With Most Water
    https://leetcode.com/problems/container-with-most-water/description/

---
+++ src/Two_Pointers/MostWater.java
@@ -0,0 +1,27 @@
+package Two_Pointers;
+
+public class MostWater {
+    class Solution {
+        public int maxArea(int[] height) {
+
+
+            height = new int[]{1,8,6,2,5,4,8,3,7};
+            int result=0;
+            int left=0;
+            int right=height.length-1;
+
+            while (left<right){
+                int area = Math.min(height[left],height[right])*(right-left);
+                result = Math.max(result,area);
+                if (left<right){
+                    left++;
+                }
+                else {
+                    right--;
+                }
+            }
+            return result;
+
+        }
+    }
+}
