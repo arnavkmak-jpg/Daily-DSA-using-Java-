5d246c06d0069b63de90449df342b2b05b829b21
arnavkmak-jpg <arnavkmak@gmail.com>
2026-09-04 09:05:17 +0530

    70. Climbing Stairs
    https://leetcode.com/problems/climbing-stairs/description/

---
+++ src/Dynamic_Programming/ClimbingStairs.java
@@ -0,0 +1,17 @@
+package Dynamic_Programming;
+
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        int one = 1;
+        int two = 1;
+
+        for (int i = 0 ; i<n-1; i++){
+            int temp = one;
+            one = one+two;
+            two = temp;
+        }
+
+        return one;
+
+    }
+}
