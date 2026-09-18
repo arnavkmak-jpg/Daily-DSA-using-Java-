3ff89ce1ce990fe794e5137ac60b0e226e354f2c
arnavkmak-jpg <arnavkmak@gmail.com>
2026-09-18 17:55:27 +0530

    344. Reverse String
    https://leetcode.com/problems/reverse-string/description/

---
+++ src/Two_Pointers/ReverseString.java
@@ -0,0 +1,18 @@
+package Two_Pointers;
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int l = 0;
+        int r = s.length-1;
+
+        while (l<r){
+            char temp = s[r];
+            s[r] = s[l];
+            s[l] = temp;
+            l++;
+            r--;
+        }
+
+
+    }
+}
