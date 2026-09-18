817a72683b361d30fd4f7fa5608c8b23c523a99a
arnavkmak-jpg <arnavkmak@gmail.com>
2026-09-18 18:26:35 +0530

    125. Valid Palindrome
    https://leetcode.com/problems/valid-palindrome/description/

---
+++ src/Two_Pointers/ValidPalindromeOptimised.java
@@ -0,0 +1,30 @@
+package Two_Pointers;
+
+public class ValidPalindromeOptimised {
+    public boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length()-1;
+
+        while (l<r){
+            while(l<r && !isAlphaNum(s.charAt(l))){
+                l++;
+            }
+            while(r>l && !isAlphaNum(s.charAt(r))){
+                r--;
+            }
+            if (Character.toLowerCase(s.charAt(l))!=Character.toLowerCase(s.charAt(r))){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    private boolean isAlphaNum(char c){
+        return (
+                c>='A' && c<='Z' || c>='a' && c<='z' || c>='0' && c<='9'
+                );
+
+    }
+}
